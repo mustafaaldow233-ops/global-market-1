9224dadef1b09d26d2370d32a446e45d05e68f9e
mustafaaldow <244281079+mustafaaldow233-ops@users.noreply.github.com>
2025-11-24 18:24:46 +0200

    Create authmidleware.js

---
+++ middleware/authmidleware.js
@@ -0,0 +1,17 @@
+// middleware/authMiddleware.js
+const jwt = require('jsonwebtoken');
+
+function authMiddleware(req, res, next){
+  const authHeader = req.headers.authorization;
+  if(!authHeader) return res.status(401).json({ message: 'No token' });
+  const token = authHeader.split(' ')[1];
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = payload; // { id, email }
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: 'Invalid token' });
+  }
+}
+
+module.exports = authMiddleware;
